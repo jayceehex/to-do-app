428eb0dc104881d55f17ab325677333b0a0b31d2
Jen Hoskins <jen.c.hoskins@gmail.com>
2018-09-12 11:31:28 +0100

    Created Tasks table and set up post functionality

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$router->group(["prefix" => "tasks"], function ($router) {
+    $router->post("", "Tasks@store");
+    $router->get("", "Tasks@store");
 });
